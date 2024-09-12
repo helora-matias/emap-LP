e8c16f0ffe994b0a1291767752cea5e6324febcd
Helora Kelly T Matias <160685192+heloktm@users.noreply.github.com>
2024-09-12 20:03:54 -0300

    Delete aula1 directory

--- aula1/helloworld.py
+++
@@ -1,9 +0,0 @@
-n = input("Digite 'a', 'b' ou 'q': ")
-while n != "q":
-    if n == "a":
-        print("Hello")
-    elif n == "b":
-        print("world!")
-    else:
-        print("Entrada inválida.")
-    n = input("Digite 'a', 'b' ou 'q': ")
